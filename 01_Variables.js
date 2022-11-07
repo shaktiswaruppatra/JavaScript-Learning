4e6d72b08deace042010eadaf828ef1aed71666d
shakti swarup patra <patrashaktiswarup@gmail.com>
2022-11-07 11:16:50 +0530

    Add files via upload

---
+++ 01_Variables.js
@@ -0,0 +1,25 @@
+console.log("This is print statement")
+var a = 67 // a contains 67
+console.log(a)
+a = "shakti"
+console.log(a)
+// let 89Shakti = 7 // Not allowed this will throw an error
+// let var = 7
+
+console.log("JavaScript tutorial: var, let and const")
+// var a = 45;
+// var a = "p"
+let b = "shakti";
+const author = "shakti"
+author = 5 // Throws an error because constant cannot be changed
+b = 4
+const shakti = 0;
+let c = null
+let d  = undefined  
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
+
+//var is Global scope where as let is of block scope.
